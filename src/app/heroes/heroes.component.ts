d04a47fcae5a32fa989df8a04f9a280b3d65f044
Rafael <rafapgbr@gmail.com>
2022-07-16 20:21:51 -0300

    feat: adicionando um observable para dados assíncronos, mesmo que nenhuma requisição assíncrona esteja sendo feita nesse momento.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero.model";
-import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,23 @@ import {HEROES} from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero; //Obtendo o "hero" do template e setando ele no objeto "selectedHero".
   }
 
-  constructor() {
+  getHeroes(): void { //Esse método obterá a lista de heróis do service.
+    this.heroService.getHeroes().subscribe(heroes =>  //Não sabemos quanto tempo vai demorar, pois estamos utilizando o "subscribe()".
+      this.heroes = heroes
+    ); //O "getHeroes()" retornará um observable de uma lista de "Hero". Estamos transformando esse observable em uma lista de "Hero".
+  }
+
+  constructor(private heroService: HeroService) { //Esse component não sabe a origem da lista de heróis, pois ele é responsável apenas por exibir, e não por obter os dados.
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 }
