f14d505ec614536e177dde70aa3d03c571cb536a
Rafael <rafapgbr@gmail.com>
2022-07-17 14:17:46 -0300

    feat: adicionando método para obter um herói único através do ID desse herói.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,4 +17,9 @@ export class HeroService {
     return heroes;
   }
 
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(hero => hero.id === id)!;
+    this.messageService.add(`Hero Service: fetched hero id=${id}.`);
+    return of(hero);
+  }
 }
