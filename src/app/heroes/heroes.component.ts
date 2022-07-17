c297f4da0bf69918c36c5fd537758839964a4403
Rafael <rafapgbr@gmail.com>
2022-07-16 21:02:09 -0300

    feat: injetando o serviço de mensagens, através do construtor, no componente "heroes".

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero.model";
 import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,17 +13,20 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) { //Esse component não sabe a origem da lista de heróis, pois ele é responsável apenas por exibir, e não por obter os dados.
+
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero; //Obtendo o "hero" do template e setando ele no objeto "selectedHero".
+    this.messageService.add("HeroesComponent: Selected Hero id =" + hero.id);
   }
 
   getHeroes(): void { //Esse método obterá a lista de heróis do service.
     this.heroService.getHeroes().subscribe(heroes =>  //Não sabemos quanto tempo vai demorar, pois estamos utilizando o "subscribe()".
       this.heroes = heroes
     ); //O "getHeroes()" retornará um observable de uma lista de "Hero". Estamos transformando esse observable em uma lista de "Hero".
-  }
-
-  constructor(private heroService: HeroService) { //Esse component não sabe a origem da lista de heróis, pois ele é responsável apenas por exibir, e não por obter os dados.
   }
 
   ngOnInit(): void {
