5d46b7ee725084e99b9e51a116a97c6006572e49
Rafael <rafapgbr@gmail.com>
2022-07-17 11:41:56 -0300

    feat: adicionando mais heróis na lista de heróis mockados.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,4 +5,8 @@ export const HEROES: Hero[] = [
   {id: 2, name: 'Hulk'},
   {id: 3, name: 'Flash'},
   {id: 4, name: 'Homem de Ferro'},
+  {id: 5, name: 'Lanterna Verde'},
+  {id: 6, name: 'Homem Formiga'},
+  {id: 7, name: 'Superman'},
+  {id: 8, name: 'Capitão América'}
 ];
