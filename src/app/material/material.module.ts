369f1348ec3e9c915a6b8cb56f5fda14d8ed051f
Rafael <rafapgbr@gmail.com>
2022-07-17 14:55:24 -0300

    feat: criando módulo apenas para inserir os módulos do Material.

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from "@angular/material/button";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatInputModule} from "@angular/material/input";
+import {MatTableModule} from "@angular/material/table";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+const MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatListModule,
+  MatInputModule,
+  MatTableModule,
+  MatIconModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MODULES
+  ],
+  exports: [
+    MODULES
+  ]
+})
+export class MaterialModule { }
