5da5e4a662d5574ce51a286cba6637a9e6417d30
Rafael <rafapgbr@gmail.com>
2022-07-17 14:18:29 -0300

    feat: adicionando método para obter um único herói através do ID desse herói e um método para retroceder uma página.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../hero.model";
+import {HeroService} from "../hero.service";
+import {Location} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,12 +11,23 @@ import {Hero} from "../hero.model";
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input()
-  selectedHero?: Hero;
+  hero!: Hero; //O "!" indica que vamos receber um valor, mas ainda não temos o valor para inserir nessa variável.
 
-  constructor() { }
+  constructor(private heroService: HeroService, //Esse serviço é necessário para buscarmos um herói.
+              private location: Location, //Essa biblioteca permite a interação com o histórico do browser.
+              private router: ActivatedRoute) { } //Essa biblioteca contém as informações sobre o momento atual da rota.
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  private getHero(): void {
+    const id = Number(this.router.snapshot.paramMap.get('id')); //Quando esse método for executado, será tirada uma "snapshot", ou seja, uma foto da URL atual e estamos obtendo o valor do parâmetro "id" dessa URL.
+
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack():void {
+    this.location.back(); //Retornando à última página do histórico do nosso browser.
+  }
 }
